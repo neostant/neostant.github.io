723e6e8417fb6d8b88a2564974ac5b5780fd61b4
Neostant <35939420+neostant@users.noreply.github.com>
2023-06-17 14:29:11 -0400

    Update hearts.js

--- src/js/hearts.js
+++ src/js/hearts.js
@@ -1,6 +1,6 @@
 // jquery ajax to http://wh.mesemi.net/neostantcom/hearts to get heart count
 $.ajax({
-    url: "http://wh.mesemi.net/neostantcom/hearts",
+    url: "https://wh.mesemi.net/neostantcom/hearts",
     success: function(data) {
         $("#hearts").html(data.hearts);
         $("#heart-ping").addClass("animate-ping")
